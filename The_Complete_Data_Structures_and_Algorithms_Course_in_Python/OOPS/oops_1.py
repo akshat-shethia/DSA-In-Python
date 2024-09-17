e8b08601ca1ff49430d3bf1ba62a65277a959569
akshat-shethia <akshatshethia@gmail.com>
2024-09-17 22:48:59 +0530

    Completed OOPS

---
+++ The_Complete_Data_Structures_and_Algorithms_Course_in_Python/OOPS/oops_1.py
@@ -0,0 +1,14 @@
+class StarCookie:
+    def __init__(self) -> None:
+        print("The cookie is ready ....")
+
+
+cookie1 = StarCookie()
+cookie1.color = "red"
+
+cookie2 = StarCookie()
+cookie2.color = "blue"
+
+
+print(cookie1.color)
+print(cookie2.color)
---
+++ The_Complete_Data_Structures_and_Algorithms_Course_in_Python/OOPS/oops_2.py
@@ -0,0 +1,12 @@
+class StarCookie:
+    def __init__(self, color) -> None:
+        print("The cookie is ready ....")
+        self.color = color
+
+
+cookie1 = StarCookie("red")
+cookie2 = StarCookie("blue")
+
+
+print(cookie1.color)
+print(cookie2.color)
