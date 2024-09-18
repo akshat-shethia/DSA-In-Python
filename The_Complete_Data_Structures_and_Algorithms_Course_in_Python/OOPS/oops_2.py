7370a9f9c1b3cea0666a4f4bc7d0b2ebc1ec9fad
akshat-shethia <akshatshethia@gmail.com>
2024-09-18 10:35:02 +0530

    Completed OOPS

--- The_Complete_Data_Structures_and_Algorithms_Course_in_Python/OOPS/oops_2.py
+++ The_Complete_Data_Structures_and_Algorithms_Course_in_Python/OOPS/oops_2.py
@@ -8,5 +8,9 @@ cookie1 = StarCookie("red")
 cookie2 = StarCookie("blue")
 
 
-print(cookie1.color)
 print(cookie2.color)
+print(cookie1.color)
+print(cookie2.__dict__)
+
+# Updates this as a new attribute and puts this as a default value for all the objects
+StarCookie.milk = 0.2
