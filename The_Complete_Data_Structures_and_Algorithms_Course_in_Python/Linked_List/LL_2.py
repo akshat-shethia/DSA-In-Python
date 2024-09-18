6c030652ea2296cb4feabf8b3841bea5d53927b3
akshat-shethia <akshatshethia@gmail.com>
2024-09-18 14:10:21 +0530

    Completed till video 139 in the LinkedList Section

--- The_Complete_Data_Structures_and_Algorithms_Course_in_Python/Linked_List/LL_2.py
+++ The_Complete_Data_Structures_and_Algorithms_Course_in_Python/Linked_List/LL_2.py
@@ -3,24 +3,46 @@ class Node:
         self.value = value
         self.next = None
 
+
 class LinkedList:
     def __init__(self) -> None:
         self.head = None
         self.tail = None
         self.length = 0
-        
+
+    def __str__(self) -> str:
+        temp = self.head
+        result = ""
+        while temp is not None:
+            result += str(temp.value)
+            if temp.next is not None:
+                result += " -> "
+            temp = temp.next
+        return result
+
     def append(self, value):
         new_node = Node(value)
         if self.head == None:
             self.head = new_node
             self.tail = new_node
-        
+
         else:
             self.tail.next = new_node
             self.tail = new_node
-        
+
         self.length += 1
-        
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+        self.length += 1
+
 
 new_linked_list = LinkedList()
 new_linked_list.append(1)
@@ -28,6 +50,7 @@ new_linked_list.append(2)
 new_linked_list.append(3)
 new_linked_list.append(4)
 new_linked_list.append(5)
+new_linked_list.prepend(70)
 
-print(f"The length of the Linked List is now -----> {new_linked_list.length}")
-        
+# print(f"The length of the Linked List is now -----> {new_linked_list.length}")
+print(new_linked_list)
