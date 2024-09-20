3aec46fc55c8489399a0c2216b08502d485ad5f7
akshat-shethia <akshatshethia@gmail.com>
2024-09-20 14:01:27 +0530

    Completed Circular Doubly Linked Lists

--- The_Complete_Data_Structures_and_Algorithms_Course_in_Python/Linked_List/DoublyLinkedListNew.py
+++ The_Complete_Data_Structures_and_Algorithms_Course_in_Python/Linked_List/DoublyLinkedListNew.py
@@ -9,11 +9,11 @@ class Node:
 
 
 class DoublyLinkedList:
-    def __init__(self, value):
-        new_node = Node(value)
-        self.head = new_node
-        self.tail = new_node
-        self.length = 1
+    # def __init__(self, value):
+    #     new_node = Node(value)
+    #     self.head = new_node
+    #     self.tail = new_node
+    #     self.length = 1
     
     def __init__(self):
         self.head = None
